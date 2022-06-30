8daf0283e1aef6e2b9524d047aa04f59cb039763
Sohail Khan <sohailjaan2002@gmail.com>
2022-06-30 18:08:36 -0400

    first commit being made

---
+++ Test.java
@@ -0,0 +1,13 @@
+
+package java18project;
+
+
+public class Test {
+
+	
+	public static void main(String[] args) {
+		System.out.println("Hello World");
+		System.out.println("Hello the weather is quite great");
+	}
+
+}
---
+++ VariableContinued.java
@@ -0,0 +1,22 @@
+package java18project;
+
+public class VariableContinued {
+
+	public static void main(String[] args) {
+		int var = 10000;
+		
+		short bigNumber = 32767;
+		
+		byte reallySmallNumber = 127;
+		
+		double decimalVariable = 234.94;
+		
+		boolean decision = false;
+		
+		char letter  = 'w';
+				
+		System.out.println(var);
+		System.out.println(reallySmallNumber);
+	}
+
+}
